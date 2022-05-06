de213025d4a071f9fc3991215ad70d7aa438507b
Alvin1213 <msalvin1995@gmail.com>
2022-05-05 20:34:45 -0400

    [MOD] HandController.cs modification
    Changes to HandController to change input from keyboard to VR

--- 007GameProject/Assets/BasicVRHands/Scripts/HandController.cs
+++ 007GameProject/Assets/BasicVRHands/Scripts/HandController.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.InputSystem;
 public class HandController : MonoBehaviour {
 
     private Animator animator;
+	private XRIDefaultInputActions inputAction;
 
-	void Start () {
+    private void Awake()
+    {
+        inputAction = new XRIDefaultInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+    void Start () {
         animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
-        animator.SetBool("isGrabbing", Input.GetKey(KeyCode.F));
+
+       if(inputAction.XRIRightHand.Grip.triggered)
+        {
+            animator.SetBool("isGrabbing",true);
+        }
+       // animator.SetBool("isGrabbing", Input.GetKey(KeyCode.F));
 	}
 }
